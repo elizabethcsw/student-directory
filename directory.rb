cb47ce0dbad9e53a6f28df512307974ed41d7f0d
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-07-18 16:16:00 +0100

    Separated data from operation and used iteration

--- directory.rb
+++ directory.rb
@@ -1,19 +1,22 @@
-student_count = 11
+
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "Overall, we have"
-print "#{student_count} great students"
-#it’s important that print() doesn’t add new line characters
-print student_count
-puts " great students"
+students.each {|s|
+  puts s}
+
+
+puts "Overall, we have #{students.count} great students"
