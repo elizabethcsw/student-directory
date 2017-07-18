b5039d63b7b311c25727cc06f6ecdace09f16b1d
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-07-18 20:41:24 +0100

    Step 8: Exercise 1

--- directory.rb
+++ directory.rb
@@ -18,18 +18,23 @@ students = [
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
+
   # create an empty array
   students = []
+
   # get the first name
   name = gets.chomp
+
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
+
     # get another name from the user
     name = gets.chomp
   end
+
   # return the array of students
   students
 end
@@ -40,8 +45,8 @@ def print_header
 end
 
 def print(names)
-names.each {|n|
-  puts "#{n[:name]} (#{n[:cohort]} cohort)"}
+names.each_with_index {|n, i|
+  puts "#{i+1}. #{n[:name]} (#{n[:cohort]} cohort)"}
 end
 
 def print_footer(names)
