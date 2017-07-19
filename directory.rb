78eef5c4a37f3ab5305249b1bfcd58d066e03c91
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-07-19 18:19:27 +0100

    Step 8: Exercise 8

--- directory.rb
+++ directory.rb
@@ -1,19 +1,3 @@
-#hash in array
-=begin
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-=end
 
 def input_students
   puts "Please enter the name of the student"
@@ -23,7 +7,7 @@ def input_students
   students = []
 
   # get the first name
-  name = gets.chomp
+  name = gets.chomp.capitalize
 
   while !name.empty? && name.length<12 do
     puts "this person's height in cm?"
@@ -34,28 +18,49 @@ def input_students
     end
 
     puts "which cohort?"
-    cohort= gets.chomp.intern
+    cohort= gets.chomp.capitalize
     while cohort.empty? do
       puts "please enter the cohort in month of this student"
-      cohort= gets.chomp.intern
+      cohort= gets.chomp.capitalize
     end
 
     students << {name: name, cohort: cohort, height: height}
     puts "Now we have #{students.count} students"
 
     # get another name from the user
-    puts "Please enter the name of the next student"
-    name = gets.chomp
+    puts "Please enter the name of the next student or hit return to finish"
+    name = gets.chomp.capitalize
   end
 
   # return the array of students
   students
 end
 
-def print_header
-  puts "The students of Villains Academy"
+def print_header(names)
+  p names
+  cohort_list=Array.new
+  names.select{|n| cohort_list << n[:cohort].to_s}
+  p cohort_list
+
+  puts "Which cohort would you like to be displayed?"
+  puts "Please pick a month: "
+  puts cohort_list.uniq.join(" ")
+
+  choice=gets.chomp.capitalize
+  while choice.empty? || !cohort_list.include?(choice) do
+    puts "Please pick a month: "
+    puts cohort_list.uniq.join(" ")
+    choice=gets.chomp.capitalize
+  end
+  puts "The students of Villains Academy in the #{choice} cohort"
   puts "-------------"
+  names.select!{|n| n[:cohort]==choice}
+puts "end"
 end
+
+=begin
+[:jan, :dec, :apr].each {|co| print "#{co.to_s} "}
+=end
 
 def print(names)
   i=0
@@ -63,13 +68,40 @@ def print(names)
     puts "#{i+1}. #{names[i][:name]} #{names[i][:height]}cm (#{names[i][:cohort]} cohort)".ljust(2)
     i+=1
   end
+  names
 end
+
+
+=begin
+names=[{:name=>"eli", :cohort=>:dec, :height=>89}, {:name=>"kate", :cohort=>:dec, :height=>39}, {:name=>"mary", :cohort=>:jul, :height=>79}]
+i=0
+while i<=names.length-1 do
+  puts "#{i+1}. #{names[i][:height]}cm"
+  i+=1
+end
+
+array1.each{|n| puts "#{n[:height]}"}
+  p names
+end
+
+
+=end
+
 
 def print_footer(names)
 puts "Overall, we have #{names.count} great students"
 end
 
 students = input_students
-print_header
+print_header(students)
 print(students)
 print_footer(students)
+
+
+
+
+#---------------------------------
+
+
+
+#---------------------------------
